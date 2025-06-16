6011f6c968574c685becd36cd3d9494df7320d3c
Tallie <84561047+Friendly-Neighbourhood-Insomniac@users.noreply.github.com>
2025-06-16 19:39:07 +0200

    fix gh-pages base and axis

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: mode === 'development' ? '/' : '/orbital-quantum-visuals/',
   server: {
     host: "::",
     port: 8080,
